b6459297e7f3b91d3e421516692e50c74fb1562b
marta_hdez90 <marta_hdez@live.com>
2020-05-19 12:02:34 +0200

    intento PyM

--- PabloyMarta/185/reverse.js
+++
@@ -1,5 +0,0 @@
-console.clear();
-var reverse = function (array) {
-    return array.reverse();
-};
-console.log(reverse([2, 4, 56, 23]));
--- PabloyMarta/185/sumacaracteres.js
+++
@@ -1,9 +0,0 @@
-var array1 = ["Casa", "Coche", "Ciudad", "Cesta"];
-var sumaArray = function (array) {
-    var result = 0;
-    for (var i = 0; i < array.length; i++) {
-        result += array[i].length;
-    }
-    return result;
-};
-console.log(sumaArray(array1));
